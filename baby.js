fb027e13d96cd8d879052df68808559ba87c36d2
Dack Ferreira <68622187+Josadack@users.noreply.github.com>
2021-10-31 16:07:22 -0300

    Site Chá do Baby

---
+++ baby.js
@@ -0,0 +1,36 @@
+
+
+
+function site01(){
+    var av1 = document.getElementById('av1')
+    var texto1 = document.getElementById('pr01')
+    var acima1 = document.getElementById('scip01')
+    
+
+    acima1.innerHTML = '<p> Escolha uma das opções e Compre o seu bilheite valor: R$20.00 </p>'
+    av1.innerHTML = '<img id="conta1" src="conta.png" width="300px">'
+    texto1.innerHTML = '<hr><p id="pix01">Número para PIX 11-9.9904-3535</p>'
+}
+
+function site02(){
+    var av2 = document.getElementById('av2')
+    var texto3 = document.getElementById('pr02')
+    var acima2 = document.getElementById('scip02')
+
+    acima2.innerHTML = '<p> Escolha uma das opções e Compre os seus dois bilheites valor: R$40.00 </p>'
+    texto3.innerHTML = '<p id="pix02">Número para PIX 11-9.9904-3535</p>'
+    av2.innerHTML = '<img id="conta2"src="conta.png" width="300px">'
+    
+}
+function site03(){
+   
+    var texto2 =  document.getElementById('pr03')
+    var acima = document.getElementById('scip03')
+    var av = document.getElementById('av')
+
+    av.innerHTML = '<img id="conta"src="conta.png" width="350px">'
+    texto2.innerHTML = '<p id="pix03">Número para PIX 11-9.9904-3535</p>'
+    acima.innerHTML = '<p> Escolha uma das opções e compre três ou mais bilhetes </p>'
+    
+}
+    
